d04008712db5770fe1ddc1a8ce20be31c58e1568
LagNeeky <lagneeky@gmail.com>
2026-09-02 10:54:28 +0200

    Update character.js

--- src/data/character.js
+++ src/data/character.js
@@ -17,6 +17,7 @@ const newCharacter = (nome = "") => ({
   competenzeGeneriche: [],
   trattiRazziali: [], privilegiClasse: [], infoExtra: [],
   incantesimiNoti: [],
+  incantesimiLivelloLancio: {},
   armiPossedute: [], armaturePossedute: [], armaturaIndossataInstId: null, accessoriPosseduti: [],
   inventario: [],
   valute: [{ id: uid(), categoria: "Oro", speso: 0, ricavato: 0, inPossesso: 0 }, { id: uid(), categoria: "Argento", speso: 0, ricavato: 0, inPossesso: 0 }, { id: uid(), categoria: "Bronzo", speso: 0, ricavato: 0, inPossesso: 0 }],
