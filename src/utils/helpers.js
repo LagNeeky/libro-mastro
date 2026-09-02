60fcdfe2311d8302616b8af4d2c1f0aa36ab5675
LagNeeky <lagneeky@gmail.com>
2026-09-02 10:49:04 +0200

    Update helpers.js

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -6,6 +6,7 @@ const PROGRESSIONE_LABELS = { nessuno: "Non incantatore", mezzo: "Mezzo incantat
 
 const MAGICO_OPTIONS = [{ id: "non_magico", nome: "Non Magico" }, { id: "magico", nome: "Magico" }];
 const POSIZIONE_OPTIONS = [{ id: "indosso", nome: "Indosso" }, { id: "zaino", nome: "Zaino" }, { id: "tasca_cintura", nome: "Tasca da Cintura" }];
+const TIPO_DANNO_OPTIONS = ["contundenti", "perforanti", "taglienti", "acido", "freddo", "fuoco", "forza", "fulmine", "necrotici", "psichici", "radiosi", "tuono", "veleno"].map((t) => ({ id: t, nome: t }));
 const RARITA_OPTIONS = [{ id: "comune", nome: "Comune" }, { id: "non_comune", nome: "Non Comune" }, { id: "raro", nome: "Raro" }, { id: "molto_raro", nome: "Molto Raro" }, { id: "leggendario", nome: "Leggendario" }, { id: "artefatto", nome: "Artefatto" }];
 const uid = () => (crypto.randomUUID ? crypto.randomUUID() : `id_${Math.random().toString(36).slice(2)}_${Date.now()}`);
 const mod = (score) => Math.floor((Number(score || 10) - 10) / 2);
@@ -38,4 +39,4 @@ const APPLICA_A_OPTIONS = (skills) => [
 
 
 
-export { ABILITIES, ABILITY_LABELS, PROGRESSIONE_LABELS, MAGICO_OPTIONS, RARITA_OPTIONS, POSIZIONE_OPTIONS, uid, mod, fmt, zeroBonus, nomeFonte, parseTratti, PROF_BONUS_BY_LEVEL, APPLICA_A_OPTIONS };
+export { ABILITIES, ABILITY_LABELS, PROGRESSIONE_LABELS, MAGICO_OPTIONS, RARITA_OPTIONS, POSIZIONE_OPTIONS, TIPO_DANNO_OPTIONS, uid, mod, fmt, zeroBonus, nomeFonte, parseTratti, PROF_BONUS_BY_LEVEL, APPLICA_A_OPTIONS };
